67822eb1d1dd55c261408c954c0ddf39b63b2b9a
Kelvin Moquete <kelvinmoquete0597@gmail.com>
2025-09-11 23:32:16 -0400

    Refactor ingredient addition logic to improve user experience

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 
 export default function Content() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const addIngredient = () => {
+    const newIngredient = document.getElementById("ingredientInput");
+    ingredients.push(newIngredient.value);
+  };
+
+  const ingredientsListItems = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+
+  function handleSubmit() {
+    console.log("Form submited!");
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. pepper"
           aria-label="Add ingredient"
+          id="ingredientInput"
         />
-        <button>Add ingredient</button>
+        <button onClick={addIngredient}>Add ingredient</button>
       </form>
+      <ul className="ingredient-list">{ingredientsListItems}</ul>
     </main>
   );
 }
